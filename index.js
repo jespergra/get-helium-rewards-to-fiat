c0fee30d09841feb0b2fcb203e4d0378612990d2
Jesper Grandien <jesperg.1997@live.se>
2022-01-28 11:50:20 +0100

    upload script

---
+++ index.js
@@ -0,0 +1,37 @@
+let today = new Date();
+const dd = String(today.getDate()).padStart(2, '0');
+const mm = String(today.getMonth() + 1).padStart(2, '0'); 
+const yyyy = today.getFullYear();
+
+// Preferbly run this script before next day to get an accurate 24h value
+today = `${yyyy}-${mm}-${dd}`;
+
+// No API-key required. You just need your hotspot address
+const res = await fetch(`https://api.helium.io/v1/hotspots/YOUR-HOTSPOT-ADDRESS/rewards/sum?min_time=${today}`);
+if (!res.ok) {
+  throw new Error(res.statusText);
+}
+
+const body = await res.json();
+
+const hntEarned = body.data.total;
+
+// Change convert to yor currency of choice
+const response = await fetch('https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?slug=helium&convert=SEK', {
+    method: 'GET',
+    headers: {
+      'X-CMC_PRO_API_KEY': 'YOUR-API-KEY',
+    }
+});
+const tbody = await response.json(); 
+// 5665 is the Helium ID
+const hntPriceInSEK = tbody.data[5665].quote.SEK.price;
+const earningsInSEK = hntEarned*hntPriceInSEK;
+// Round earnings
+const roundedEarnings = Math.round(earningsInSEK * 100) / 100;
+console.log(roundedEarnings);
+
+return roundedEarnings;
+
+
+
